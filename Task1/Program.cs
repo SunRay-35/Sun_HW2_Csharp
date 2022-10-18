a515919454522f9a2d04ad295c975221e3b5e74e
SunRay-35 <siaip@mail.ru>
2022-10-18 22:31:15 +0300

    Сдаем домашнее задание

---
+++ Task1/Program.cs
@@ -0,0 +1,57 @@
+﻿// Задача 1: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа. Не использовать строки для расчета.
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+int PromptInt(string strDescription) // Ввод целого числа
+{
+    try
+    {
+        System.Console.Write(strDescription);
+        int intTemp = int.Parse(System.Console.ReadLine());
+        return intTemp;
+    }
+    catch
+    {
+        System.Console.WriteLine("Введено некорректное значение, попробуйте снова.");
+        int intException = PromptInt(strDescription);
+        return intException;
+    }
+}
+
+bool IsDigitCapacityRight(int intNumber, int intCapacity) // Проверка количества разрядов
+{
+    int intTemp = Math.Abs(intNumber);
+    if (intCapacity > 0)
+    {
+        double dblPower = Convert.ToDouble(intCapacity);
+        return (intTemp >= Math.Pow(10.0, dblPower - 1) && intTemp < Math.Pow(10.0, dblPower));
+    }
+    else
+    {
+        System.Console.WriteLine($"Разрядность при вызову функции isDigitCapacityRight указана неверно, ожидается натуральное число (1, 2, 3 ..), а указано {intCapacity}");
+        return false;
+    }
+}
+
+int TakeLastDigit(int intNumber) // Возвращает остаток деления целого числа на 10
+{
+    return Math.Abs((intNumber % 10));
+}
+
+int ReduceCapacity(int intNumber) // Уменьшает разрядность целого числа на 1
+{
+    return (intNumber / 10);
+}
+
+System.Console.Clear();
+int intNumber = PromptInt("Введите целое трехзначное число: ");
+System.Console.WriteLine(intNumber);
+if (IsDigitCapacityRight(intNumber, 3))
+{
+    System.Console.WriteLine($"Вторая цифра трехзначного числа {intNumber} равна {TakeLastDigit(ReduceCapacity(intNumber))}");
+}
+else
+{
+    System.Console.WriteLine("Введено не трехзначное число, попробуйте снова.");
+}
---
+++ Task4/Program.cs
@@ -0,0 +1,50 @@
+﻿// Задача 4: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+// 6 -> да
+// 7 -> да
+// 1 -> нет
+
+int PromptInt(string strDescription) // Ввод целого числа
+{
+    try
+    {
+        System.Console.Write(strDescription);
+        int intTemp = int.Parse(System.Console.ReadLine());
+        return intTemp;
+    }
+    catch
+    {
+        System.Console.WriteLine("Введено некорректное значение, попробуйте снова.");
+        int intException = PromptInt(strDescription);
+        return intException;
+    }
+}
+
+bool IsDayIsWeekend(int intNumberOfDay)
+{
+    if (intNumberOfDay < 1 || intNumberOfDay >7)
+    {
+        System.Console.Write("Введено некорректное значение дня недели.");
+        return false;
+    }
+    else if (intNumberOfDay < 6)
+    {
+        System.Console.WriteLine("Нет, это рабочий день.");
+        return false;
+    }
+    else
+    {
+        System.Console.WriteLine("Да! Это выходной!");
+        return true;
+    }
+}
+
+System.Console.Clear();
+int intNumber = PromptInt("Введите порядковый номер дня недели (где 1 - понедельник, а 7 - воскресенье): ");
+    if (IsDayIsWeekend(intNumber))
+    {
+       // Просто согласимся с ответом, а при необходимости можем обработать результат
+    }
+    else
+    {
+        // Просто согласимся с ответом, а при необходимости можем обработать результат
+    }
